2806782b0fadba61b5ec788b1ea8c34fc708648d
erleibiazzio <erleibiazzio@gmail.com>
2021-09-07 09:30:13 -0300

    Ajusta interface para tradução e resolve problema que oculta texto Processar do botão

--- layouts/parts/validator/validator-uploads.php
+++ layouts/parts/validator/validator-uploads.php
@@ -1,11 +1,13 @@
 <?php
+use \MapasCulturais\i;
+
 $app = MapasCulturais\App::i();
 $slug = $plugin->getSlug();
 $name = $plugin->getName();
 $files = $entity->getFiles($slug);
 $url = $app->createUrl($slug, "import", ["opportunity" => $entity->id]);
-$msg_delete = \MapasCulturais\i::__("Remover este arquivo?");
-$btn_delete = \MapasCulturais\i::__("Excluir arquivo");
+$msg_delete = i::__("Remover este arquivo?");
+$btn_delete = i::__("Excluir arquivo");
 $template = '
 <li id="file-{{id}}" class="widget-list-item">
     <a href="{{url}}" rel="noopener noreferrer">{{description}}</a>
@@ -18,21 +20,24 @@ $template = '
 <div class="widget">
     <h3 class="editando"><?= sprintf(\MapasCulturais\i::__("Arquivos do %s"), $name) ?></h3>
     <div>
-        <a class="add js-open-editbox hltip" data-target="#editbox-<?= $slug ?>-file" href="#" title="<?= sprintf(\MapasCulturais\i::__("Clique para adicionar subir novo arquivo de validação do %s"), $name) ?>"> <?= \MapasCulturais\i::__("subir arquivo") ?></a>
+        <a class="add js-open-editbox hltip" data-target="#editbox-<?= $slug ?>-file" href="#" title="<?= sprintf(\MapasCulturais\i::__("Clique para adicionar subir novo arquivo de validação do %s"), $name) ?>"> <?= i::__("subir arquivo") ?></a>
     </div>
-    <div id="editbox-<?= $slug ?>-file" class="js-editbox mc-left" title="<?= sprintf(\MapasCulturais\i::__("Subir arquivo de validação do %s"), $name) ?>" data-submit-label="<?= \MapasCulturais\i::__("Enviar") ?>">
+    <div id="editbox-<?= $slug ?>-file" class="js-editbox mc-left" title="<?= sprintf(\MapasCulturais\i::__("Subir arquivo de validação do %s"), $name) ?>" data-submit-label="<?= i::__("Enviar") ?>">
         <?php $this->ajaxUploader($entity, $slug, "append", "ul.js-validador", $template, "", false, false, false); ?>
     </div>
     <ul class="widget-list js-validador js-slimScroll">
         <?php if (is_array($files)): foreach( $files as $file): ?>
-            <li id="file-<?= $file->id ?>" class="widget-list-item<?php if ($this->isEditable()) echo \MapasCulturais\i::__(" is-editable"); ?>" >
+            <li id="file-<?= $file->id ?>" class="widget-list-item<?php if ($this->isEditable()) echo i::__(" is-editable"); ?>" >
                 <a href="<?= $file->url ?>"><span><?= $file->description ? $file->description : $file->name ?></span></a>
                 <?php if ($processed_at = $entity->{$slug . '_processed_files'}->{$file->name} ?? null): ?>
                     - <?= sprintf(\MapasCulturais\i::__("processado em %s"), $processed_at) ?>
                 <?php else: ?>
                 <div class="botoes">
-                    <a href="<?=$url?>?file=<?=$file->id?>" class="btn btn-primary hltip js-validador-process" data-hltip-classes="hltip-ajuda" title="<?= \MapasCulturais\i::__("Clique para processar o arquivo enviado") ?>"><? \MapasCulturais\i::__("processar") ?></a>
-                    <a data-href="<?= $file->deleteUrl ?>" data-target="#file-<?= $file->id ?>" data-configm-message="<?= \MapasCulturais\i::__("Remover este arquivo?") ?>" class="delete hltip js-remove-item" data-hltip-classes="hltip-ajuda" title="<?= \MapasCulturais\i::__("Excluir arquivo. Só é possível fazer esta ação antes do processamento.") ?>"></a>
+                    <a href="<?=$url?>?file=<?=$file->id?>" class="btn btn-primary hltip js-validador-process" data-hltip-classes="hltip-ajuda" title="<?= i::__("Clique para processar o arquivo enviado") ?>">
+                        <?= i::__("processar") ?>
+                    </a>
+                    
+                    <a data-href="<?= $file->deleteUrl ?>" data-target="#file-<?= $file->id ?>" data-configm-message="<?= i::__("Remover este arquivo?") ?>" class="delete hltip js-remove-item" data-hltip-classes="hltip-ajuda" title="<?= i::__("Excluir arquivo. Só é possível fazer esta ação antes do processamento.") ?>"></a>
                 </div>
                 <?php endif; ?>
             </li>
